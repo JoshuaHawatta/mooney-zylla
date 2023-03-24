b2fdb40ca75e696233ce524a929f05d7b2b69ba8
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-24 19:52:45 -0300

    implements: created a Spring Component for handling validations of my services

---
+++ src/main/java/com/joshuahawatta/moneyzilla/entities/validations/Validations.java
@@ -0,0 +1,22 @@
+package com.joshuahawatta.moneyzilla.entities.validations;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ValidationException;
+import jakarta.validation.Validator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Set;
+
+@Component
+public class Validations {
+    @Autowired
+    Validator validator;
+
+    private Validations() {}
+
+    public <T> void setValidations(T entity) {
+        Set<ConstraintViolation<T>> violations = validator.validate(entity);
+        if (!violations.isEmpty()) throw new ValidationException("Usuário inválido!", (Throwable) violations);
+    }
+}
