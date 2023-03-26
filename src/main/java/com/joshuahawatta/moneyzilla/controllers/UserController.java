17de64bd8e4344036f1ffcd65da95e5dee23a1e5
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-25 23:59:30 -0300

    implements: created my user controller

---
+++ src/main/java/com/joshuahawatta/moneyzilla/controllers/UserController.java
@@ -0,0 +1,45 @@
+package com.joshuahawatta.moneyzilla.controllers;
+
+import com.joshuahawatta.moneyzilla.dtos.user.UserDto;
+import com.joshuahawatta.moneyzilla.entities.responses.Response;
+import com.joshuahawatta.moneyzilla.entities.responses.ResponseResult;
+import com.joshuahawatta.moneyzilla.entities.responses.ResponseResultWIthMessage;
+import com.joshuahawatta.moneyzilla.models.User;
+import com.joshuahawatta.moneyzilla.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService service;
+
+    @GetMapping
+    public ResponseEntity<Response<List<UserDto>>> findAll() {
+        return Response.sendResponse(new ResponseResult<>(200, service.findAll()));
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<Response<UserDto>> findById(@PathVariable Long id) {
+        return Response.sendResponse(new ResponseResult<>(200, service.findById(id)));
+    }
+
+    @GetMapping(value = "login/{email}")
+    public ResponseEntity<Response<UserDto>> findByEmail(@RequestBody User user) {
+        return Response.sendResponse(new ResponseResult<>(200, service.login(user)));
+    }
+
+    @PostMapping(value = "register")
+    public ResponseEntity<Response<UserDto>> save(@RequestBody User user) {
+        UserDto newUser = service.save(user);
+
+        return Response.sendResponse(
+            new ResponseResultWIthMessage<>(201, newUser, "Olá, " + newUser.getName() + "!")
+        );
+    }
+
+}
