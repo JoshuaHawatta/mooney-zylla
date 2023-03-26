ac8a60be804d7e9161bc4a8173d1beb5d29bb784
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-03-25 23:57:47 -0300

    chore: created a method for getting users by email

--- src/main/java/com/joshuahawatta/moneyzilla/repositories/UserRepository.java
+++ src/main/java/com/joshuahawatta/moneyzilla/repositories/UserRepository.java
@@ -2,5 +2,8 @@ package com.joshuahawatta.moneyzilla.repositories;
 
 import com.joshuahawatta.moneyzilla.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {}
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
