14f27195b92f92d02abc58f5e8023e6ca2e86652
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-05-17 02:08:59 -0300

    chore: added documentation for BaseEntityModel class

--- src/main/java/com/joshuahawatta/moneyzilla/entities/baseentitymodel/BaseEntityModel.java
+++ src/main/java/com/joshuahawatta/moneyzilla/entities/baseentitymodel/BaseEntityModel.java
@@ -5,29 +5,39 @@ import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import java.time.LocalDateTime;
 
+/**
+ * A basic Entity that won´t be created at database that will handle the createdAt and updatedAt fields for all database models.
+ */
 @MappedSuperclass
 public abstract class BaseEntityModel {
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    @Column(name = "created_at", nullable = false)
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
+    /**
+     * Just before an entity is registered on the database, the createdAt and updatedAt fields will be created.
+     */
     @PrePersist
     protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
     }
 
+    /**
+     * Just before an entity is updated on the database, the updatedAt field will be remade with a new LocalDateTime.
+     */
     @PreUpdate
     protected void onUpdate() { updatedAt = LocalDateTime.now(); }
 
-    //GETTERS_AND_SETTERS
+    /**
+     * Getters and setters.
+     */
     public LocalDateTime getCreatedAt() { return createdAt; }
 
     public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
