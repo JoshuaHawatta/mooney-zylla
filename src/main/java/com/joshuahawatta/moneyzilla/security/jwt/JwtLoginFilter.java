e6a18b8935189a53654c4bef8c8c54817306984e
Joshua Hawatta <joshuahawattaazevedo@gmail.com>
2023-05-17 23:56:26 -0300

    implements: created a login filter for JWT

---
+++ src/main/java/com/joshuahawatta/moneyzilla/security/jwt/JwtLoginFilter.java
@@ -0,0 +1,67 @@
+package com.joshuahawatta.moneyzilla.security.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.joshuahawatta.moneyzilla.models.Users;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import java.io.IOException;
+
+/**
+ * Stabilishes the Token managment/manager
+ */
+public class JwtLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+
+    /**
+     * Oblige the constructor to always authenticate URL.
+     * @param url get the URL that is trying to authenticate
+     * @param authenticationManager only to set the authenticationManager for handling all the authentications.
+     */
+    protected JwtLoginFilter(String url, AuthenticationManager authenticationManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authenticationManager);
+    }
+
+    /**
+     * @param req used to get the user by it´s token.
+     * @param res only there to complete the @Override annotation and not throwing any errors.
+     *@return the processed user after process the authentication.
+     */
+    @Override
+    public Authentication attemptAuthentication(
+        HttpServletRequest req,
+        HttpServletResponse res
+    ) throws AuthenticationException, IOException {
+        Users user = new ObjectMapper().readValue(req.getInputStream(), Users.class);
+
+        return getAuthenticationManager()
+            .authenticate(
+                new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword(), user.getAuthorities())
+            );
+    }
+
+    /**
+     * get the logged user by the token of the response.
+     * @param req only there to complete the @Override annotation and not throwing any errors.
+     * @param res get the user by the response.
+     * @param filterChain only there to complete the @Override annotation and not throwing any errors.
+     * @param authentication help getting user data from the token.
+     * @throws IOException only there to complete the @Override annotation and not throwing any errors.
+     */
+    @Override
+    protected void successfulAuthentication(
+        HttpServletRequest req,
+        HttpServletResponse res,
+        FilterChain filterChain, Authentication authentication
+    ) throws IOException {
+        new JwtAuthenticationService().generateToken(res, authentication.getName());
+    }
+}
