ddc4152cb23013395dbe688639f8fc541a356d83
JoshuaHawatta <joshuahawatta@gmail.com>
2023-08-03 04:10:17 -0300

    chore: minor changes on Validations.java

--- src/main/java/com/joshuahawatta/moneyzilla/configurations/validations/Validations.java
+++ src/main/java/com/joshuahawatta/moneyzilla/configurations/validations/Validations.java
@@ -3,16 +3,16 @@ package com.joshuahawatta.moneyzilla.configurations.validations;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ValidationException;
 import jakarta.validation.Validator;
+import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@NoArgsConstructor
 public class Validations {
     @Autowired
     Validator validator;
-
-    private Validations() {}
 
     public <T> void validate(T entity) {
         List<ConstraintViolation<T>> violations = validator.validate(entity).stream().toList();
