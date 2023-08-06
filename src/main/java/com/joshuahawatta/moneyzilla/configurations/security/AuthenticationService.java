f1c2b01ac9d3fbf019fb9500643bf6205439c89e
JoshuaHawatta <joshuahawatta@gmail.com>
2023-08-06 01:31:26 -0300

    chore: minor changes at Authentication Service

--- src/main/java/com/joshuahawatta/moneyzilla/configurations/security/AuthenticationService.java
+++ src/main/java/com/joshuahawatta/moneyzilla/configurations/security/AuthenticationService.java
@@ -2,7 +2,6 @@ package com.joshuahawatta.moneyzilla.configurations.security;
 
 import com.joshuahawatta.moneyzilla.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
